89065a0b4a1e27409c747260a3f149bc51055b04
Ila-bura <>
2023-05-22 19:09:40 +0000

    Add comments with functions needed in JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// Array with ten sets of questions with their corresponding possible options
+
+
+// Variable for the question
+
+
+// Variable for the answer
+
+
+// Variable for the next button
+
+
+
+// Variable to store the score index an initial score
+
+
+// Function to start the quiz with question index and initial score set to 0
+
+
+// Function to display the questions
+
+// Function to check if answer is correct 
+
+// Function to clear the area
+
+// Function to display the final score
+
+
+// Function to restart the quiz
